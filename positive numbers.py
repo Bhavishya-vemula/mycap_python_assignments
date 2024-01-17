6778d2f547fceb1ae4db3aaa2076e054c275a5cd
Bhavishya-vemula <142679694+Bhavishya-vemula@users.noreply.github.com>
2024-01-17 23:10:56 +0530

    Update positive numbers.py

--- positive numbers.py
+++ positive numbers.py
@@ -1,10 +1,8 @@
-# This Python program prints all positive numbers in a range.
+# all positive numbers in a range
 
-# Define the start and end of the range.
 start = int(input("Enter the start of the range: "))
 end = int(input("Enter the end of the range: "))
 
-# Iterate over the range and print all positive numbers.
 for i in range(start, end + 1):
     if i >= 0:
         print(i)
