4c10389f1b70cf260158212586ebdc9cbd2e7918
Bhavishya-vemula <142679694+Bhavishya-vemula@users.noreply.github.com>
2024-01-18 22:02:36 +0530

    Update set operations.py

--- set operations.py
+++ set operations.py
@@ -1,17 +1,16 @@
-# Program to perform different set operations like in mathematics
+# different set operations 
 
-# define three sets
 E = {0, 2, 4, 6, 8};
 N = {1, 2, 3, 4, 5};
 
-# set union
+# union
 print("Union of E and N is",E | N)
 
-# set intersection
+# intersection
 print("Intersection of E and N is",E & N)
 
-# set difference
+# difference
 print("Difference of E and N is",E - N)
 
-# set symmetric difference
+# symmetric difference
 print("Symmetric difference of E and N is",E ^ N)
